06947afa373c24820c79b854a53d6c1129888fc8
cortadocodes <cortado.codes@protonmail.com>
2025-01-15 18:04:37 +0000

    OPS: Add initial terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~>6.12"
+    }
+  }
+  cloud {
+    organization = "octue"
+    workspaces {
+      project = "octue-sdk-python"
+      tags = ["example-service-kueue"]
+    }
+  }
+}
+
+
+provider "google" {
+  credentials = file(var.google_cloud_credentials_file)
+  project     = var.google_cloud_project_id
+  region      = var.google_cloud_region
+}
+
+
+module "octue_twined" {
+  source = "git::github.com/octue/terraform-octue-twined.git?ref=create-initial-module"
+
+  google_cloud_project_id = var.google_cloud_project_id
+  google_cloud_project_number = var.google_cloud_project_number
+  google_cloud_region = var.google_cloud_region
+  github_organisation = var.github_organisation
+  environment = terraform.workspace
+  twined_service_namespace = var.twined_service_namespace
+  service_account_names = var.service_account_names
+  deletion_protection = var.deletion_protection
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "google_cloud_project_id" {
+  type = string
+  default = "terraform-training-438911"
+}
+
+variable "google_cloud_project_number" {
+  type = string
+  default = "1032632085173"
+}
+
+variable "google_cloud_region" {
+  type = string
+  default = "europe-west1"
+}
+
+variable "github_organisation" {
+  type = string
+  default = "octue"
+}
+
+variable "twined_service_namespace" {
+  type = string
+  default = "octue"
+}
+
+variable "service_account_names" {
+  type = set(string)
+  default = ["cortadocodes", "thclark"]
+}
+
+variable "google_cloud_credentials_file" {
+  type    = string
+  default = "gcp-credentials.json"
+}
+
+variable "deletion_protection" {
+  type    = bool
+  default = false
+}
